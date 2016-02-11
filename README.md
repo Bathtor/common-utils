7579aa8d353c26d3931ccba278cd45dfb8d79197
Lars Kroll <lkroll@kth.se>
2016-02-11 15:15:47 +0100

    added EMA implementation to Java

--- README.md
+++ README.md
@@ -2,3 +2,5 @@ CommonUtils
 ============
 
 This is just a quick collection of things that I see coming up over and over again in my projects.
+
+CurrentVersion: 1.1
---
+++ java/src/main/java/com/larskroll/common/statistics/ExponentialMovingAverage.java
@@ -0,0 +1,54 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.larskroll.common.statistics;
+
+/**
+ * Implements the simple Exponential Moving Average (EMA) with decay coefficient {@code alpha}.
+ * 
+ * {@code alpha} should be to be a finite number between 0.0 and 1.0;
+ * 
+ * @author lkroll
+ */
+public class ExponentialMovingAverage {
+    private final double alpha; // decay
+    private double mu = Double.NaN; // current mean approximation
+    
+    /**
+     * 
+     * @param alpha decay coefficient
+     */
+    public ExponentialMovingAverage(double alpha) {
+        if (!Double.isFinite(alpha)) {
+            throw new RuntimeException("alpha must be a finite value!");
+        }
+        this.alpha = alpha;
+    }
+    
+    /**
+     * Add value to EMA.
+     * 
+     * @param x is added to the moving average
+     */
+    public void addValue(double x) {
+        if (Double.isNaN(x)) {
+            return; // can't add NaNs
+        }
+        if (Double.isNaN(mu)) {
+            mu = x; // just take the new value as you can't calculate with NaN
+            return;
+        }
+        mu = mu + alpha*(x-mu);
+    }
+    
+    /**
+     * Get the current EMA.
+     * 
+     * @return the mean estimate
+     */
+    public double getMean() {
+        return mu;
+    }
+}
